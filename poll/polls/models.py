c04edaddb715c53d9050d35a8e00ea34f2317bf6
700brains <myprogramminglive@gmail.com>
2021-10-25 12:21:01 -0400

    more model codes

--- poll/polls/models.py
+++ poll/polls/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now()-datetime.timedelta(dats=1)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('DATE PUBLISHED')
 
 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text= models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
